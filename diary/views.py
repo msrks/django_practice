f1997001a1c1632e517b936325f927195cb0cf8b
msrks <mshrrks@gmail.com>
2018-01-14 16:03:59 +0900

    add update function to diary app

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DayCreateForm
 from .models import Day
 
@@ -10,6 +10,20 @@ def index(request):
 
 def add(request):
     form = DayCreateForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'diary/day_form.html', context)
+
+def update(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    form = DayCreateForm(request.POST or None, instance=day)
 
     if request.method == 'POST' and form.is_valid():
         form.save()
